16d766801e2893a6cee814a8469a3f61cfb2a330
Kaue Oliveira <kauebenk@gmail.com>
2024-08-31 12:49:08 -0300

    refactor - improve ClienteController and IController

--- src/main/java/org/kauebenk/backendecommercedecartasapp/controller/ClienteController.java
+++ src/main/java/org/kauebenk/backendecommercedecartasapp/controller/ClienteController.java
@@ -3,43 +3,75 @@ package org.kauebenk.backendecommercedecartasapp.controller;
 import org.kauebenk.backendecommercedecartasapp.DAO.ClienteDAO;
 import org.kauebenk.backendecommercedecartasapp.dominio.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
-public class ClienteController {
+public class ClienteController implements IController<Cliente> {
 
     @Autowired
     private ClienteDAO clienteDAO;
 
+    @Override
     @PostMapping
-    public void salvar(@RequestBody Cliente cliente) {
-        clienteDAO.salvar(cliente);
+    public ResponseEntity<Void> salvar(@RequestBody Cliente cliente) {
+        try {
+            clienteDAO.salvar(cliente);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @Override
     @PutMapping
-    public void alterar(@RequestBody Cliente cliente) {
-        clienteDAO.alterar(cliente);
+    public ResponseEntity<Void> alterar(@RequestBody Cliente cliente) {
+        try {
+            clienteDAO.alterar(cliente);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @Override
     @DeleteMapping("/{id}")
-    public void excluir(@PathVariable Long id) {
-        Cliente cliente = new Cliente();
-        cliente.setId(id);
-        clienteDAO.excluir(cliente);
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        try {
+            Cliente cliente = new Cliente();
+            cliente.setId(id);
+            clienteDAO.excluir(cliente);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @Override
     @GetMapping("/{id}")
-    public Cliente consultar(@PathVariable Long id) {
-        Cliente cliente = new Cliente();
-        cliente.setId(id);
-        return clienteDAO.consultar(cliente);
+    public ResponseEntity<Cliente> consultar(@PathVariable Long id) {
+        try {
+            Cliente cliente = new Cliente();
+            cliente.setId(id);
+            Cliente result = clienteDAO.consultar(cliente);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @Override
     @GetMapping
-    public List<Cliente> listar() {
-        return clienteDAO.listar(new Cliente());
+    public ResponseEntity<List<Cliente>> listar() {
+        try {
+            List<Cliente> clientes = clienteDAO.listar(new Cliente());
+            return new ResponseEntity<>(clientes, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/org/kauebenk/backendecommercedecartasapp/controller/IController.java
+++ src/main/java/org/kauebenk/backendecommercedecartasapp/controller/IController.java
@@ -1,11 +1,19 @@
 package org.kauebenk.backendecommercedecartasapp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 public interface IController<EntidadeDominio> {
-    public void salvar(EntidadeDominio entidade);
-    public void alterar(EntidadeDominio entidade);
-    public void excluir(EntidadeDominio entidade);
-    public EntidadeDominio consultar(EntidadeDominio entidade);
-    public List<EntidadeDominio> listar(EntidadeDominio entidade);
+    @PostMapping
+    ResponseEntity<Void> salvar(EntidadeDominio entidade);
+    @PutMapping
+    ResponseEntity<Void> alterar(EntidadeDominio entidade);
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> excluir(@PathVariable Long id);
+    @GetMapping("{id}")
+    ResponseEntity<EntidadeDominio> consultar(@PathVariable Long id);
+    @GetMapping
+    ResponseEntity<List<EntidadeDominio>> listar();
 }
